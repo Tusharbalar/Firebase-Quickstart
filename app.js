5ff185f071530e328fdb2f267b677dd1dd08ff98
TusharBalar <balar.tushar1@gmail.com>
2017-01-08 17:52:28 +0530

    Login user able to upload a file

--- app.js
+++ app.js
@@ -84,3 +84,29 @@ firebase.auth().onAuthStateChanged(firebaseUser => {
 btnLogout.addEventListener('click', e => {
   firebase.auth().signOut();
 });
+
+var uploader = document.getElementById("uploader");
+var fileButton = document.getElementById("fileButton");
+
+fileButton.addEventListener('change', e => {
+
+  // Get fileButton
+  var file = e.target.files[0];
+
+  // Create storage ref
+  var storageRef = firebase.storage().ref("sweet_gifs/" + file.name);
+
+  // Upload a fileButton
+  var task = storageRef.put(file);
+
+  // Update progress bar
+  task.on('state_changed', function progress(snapshot) {
+    var percentage = (snapshot.bytesTransferred / snapshot.totalBytes ) * 100;
+    uploader.value = percentage;
+  }, function() {
+
+  }, function() {
+
+  });
+
+});
